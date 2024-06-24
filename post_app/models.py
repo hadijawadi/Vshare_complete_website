1ba4c873e91a30afc93702856e41fa308e4e4cca
hadi_javad <haadi@gmail.com>
2024-06-24 00:07:19 -0700

    user create text post created the models and registerd to admin panel

--- post_app/models.py
+++ post_app/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 
 
+class Create_text_post(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.user
